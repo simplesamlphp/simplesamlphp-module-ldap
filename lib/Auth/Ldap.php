9c11f94469ba78fe9455f5fdfa94036918a5bdd0
Tim van Dijen <tim.dijen@minbzk.nl>
2020-05-13 20:11:28 +0200

    Fix typo

--- lib/Auth/Ldap.php
+++ lib/Auth/Ldap.php
@@ -487,7 +487,7 @@ class Ldap
 
                 // decide whether to base64 encode or not
                 for ($k = 0; $k < $attribute['count']; $k++) {
-                    $value = $attributes[$k];
+                    $value = $attribute[$k];
 
                     // base64 encode binary attributes
                     if (
