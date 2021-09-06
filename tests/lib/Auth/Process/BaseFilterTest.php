a19652c112dcb2520fec7699a44bbdc3b32c2f99
Tim van Dijen <tvdijen@gmail.com>
2021-09-06 14:13:27 +0200

    Fix test

--- tests/lib/Auth/Process/BaseFilterTest.php
+++ tests/lib/Auth/Process/BaseFilterTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SimpleSAML\Test\Module\ldap\Auth\Process;
 
 use PHPUnit\Framework\TestCase;
+use ReflectionClass;
 use SimpleSAML\Module\ldap\Auth\Process\BaseFilter;
 
 class BaseFilterTest extends TestCase
@@ -16,16 +17,15 @@ class BaseFilterTest extends TestCase
         $stub = $this->getMockBuilder(BaseFilter::class)
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
-        $class = new \ReflectionClass($stub);
+        $class = new ReflectionClass($stub);
         $method = $class->getMethod('varExport');
         $method->setAccessible(true);
 
         $this->assertEquals(
-            "array ( 'ldap.hostname' => 'ldap://172.17.101.32', 'ldap.port' => 389, 'ldap.password' => '********', )",
+            "array ( 'connection_string' => 'ldap://172.17.101.32:389', 'search.password' => '********', )",
             $method->invokeArgs($stub, [[
-                'ldap.hostname' => 'ldap://172.17.101.32',
-                'ldap.port' => 389,
-                'ldap.password' => 'password',
+                'connection_string' => 'ldap://172.17.101.32:389',
+                'search.password' => 'password',
             ]])
         );
     }
