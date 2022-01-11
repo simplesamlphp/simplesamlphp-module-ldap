simplesamlphp/simplesamlphp-module-ldap

commit 38ad58c940523937f3019e749614ac12f7da9744

Add search filters to AttributeAddUsersGroups (#27)
* Add search filters to AttributeAddUsersGroups

* Allow returning something other than a group's DN

lib/Auth/Process/AttributeAddUsersGroups.php
@@ -20,6 +20,15 @@ use SimpleSAML\Utils;
 class AttributeAddUsersGroups extends BaseFilter
 {
     /**
+     * LDAP search filters to be added to the base filters for this authproc-filter.
+     * It's an array of key => value pairs that will be translated to (key=value) in the ldap query.
+     *
+     * @var array
+     */
+    protected array $additional_filters;
+
+
+    /**
      * This is run when the filter is processed by SimpleSAML.
      * It will attempt to find the current users groups using
      * the best method possible for the LDAP product. The groups
@@ -36,6 +45,8 @@ class AttributeAddUsersGroups extends BaseFilter
         Logger::debug(
             $this->title . 'Attempting to get the users groups...'
         );
+
+        $this->additional_filters = $this->config->getArray('additional_filters', []);
 
         // Reference the attributes, just to make the names shorter
         $attributes = &$request['Attributes'];
@@ -167,8 +178,13 @@ class AttributeAddUsersGroups extends BaseFilter
              */
             $all_groups = $this->getLdap()->searchformultiple(
                 $openldap_base,
-                [$map['memberof'] => $attributes[$map['username']][0]],
-                [$map['member']]
+                array_merge(
+                    [
+                        $map['memberof'] => $attributes[$map['username']][0]
+                    ],
+                    $this->additional_filters
+                ),
+                [$map['return']]
             );
         } catch (Error\UserNotFound $e) {
             return $groups; // if no groups found return with empty (still just initialized) groups array
@@ -176,7 +192,7 @@ class AttributeAddUsersGroups extends BaseFilter
 
         // run through all groups and add each to our groups array
         foreach ($all_groups as $group_entry) {
-            $groups[] = $group_entry[$map['member']][0];
+            $groups[] = $group_entry[$map['return']][0];
         }
 
         return $groups;
@@ -320,6 +336,7 @@ class AttributeAddUsersGroups extends BaseFilter
             $this->title . 'Searching ActiveDirectory group membership.' .
             ' DN: ' . $dn .
             ' DN Attribute: ' . $map['dn'] .
+            ' Return Attribute: ' . $map['return'] .
             ' Member Attribute: ' . $map['member'] .
             ' Type Attribute: ' . $map['type'] .
             ' Type Value: ' . $this->type_map['group'] .
@@ -333,8 +350,14 @@ class AttributeAddUsersGroups extends BaseFilter
         try {
             $entries = $this->getLdap()->searchformultiple(
                 $this->base_dn,
-                [$map['type'] => $this->type_map['group'], $map['member'] . ':1.2.840.113556.1.4.1941:' => $dn],
-                [$map['dn']]
+                array_merge(
+                    [
+                        $map['type'] => $this->type_map['group'],
+                        $map['member'] . ':1.2.840.113556.1.4.1941:' => $dn
+                    ],
+                    $this->additional_filters
+                ),
+                [$map['return']]
             );
 
         // The search may throw an exception if no entries
@@ -349,27 +372,27 @@ class AttributeAddUsersGroups extends BaseFilter
         // Check each entry..
         foreach ($entries as $entry) {
             // Check for the DN using the original attribute name
-            if (isset($entry[$map['dn']][0])) {
-                $groups[] = $entry[$map['dn']][0];
+            if (isset($entry[$map['return']][0])) {
+                $groups[] = $entry[$map['return']][0];
                 continue;
             }
 
             // Sometimes the returned attribute names are lowercase
-            if (isset($entry[strtolower($map['dn'])][0])) {
-                $groups[] = $entry[strtolower($map['dn'])][0];
+            if (isset($entry[strtolower($map['return'])][0])) {
+                $groups[] = $entry[strtolower($map['return'])][0];
                 continue;
             }
 
             // AD queries also seem to return the objects dn by default
-            if (isset($entry['dn'])) {
-                $groups[] = $entry['dn'];
+            if (isset($entry['return'])) {
+                $groups[] = $entry['return'];
                 continue;
             }
 
             // Could not find DN, log and continue
             Logger::notice(
-                $this->title . 'The DN attribute [' .
-                implode(', ', [$map['dn'], strtolower($map['dn']), 'dn']) .
+                $this->title . 'The return attribute [' .
+                implode(', ', [$map['return'], strtolower($map['return'])]) .
                 '] could not be found in the entry. ' . $this->varExport($entry)
             );
         }

lib/Auth/Process/BaseFilter.php
@@ -154,6 +154,7 @@ abstract class BaseFilter extends \SimpleSAML\Auth\ProcessingFilter
             'member'   => $this->config->getString('attribute.member', 'member'),
             'memberof' => $this->config->getString('attribute.memberof', 'memberOf'),
             'name'     => $this->config->getString('attribute.groupname', 'name'),
+            'return'   => $this->config->getString('attribute.return', 'distinguishedName'),
             'type'     => $this->config->getString('attribute.type', 'objectClass'),
             'username' => $this->config->getString('attribute.username', 'sAMAccountName')
         ];

commit 6a548e6cf2cd95b62feb3907cb534d23cf44d87e

Fix bug introduced in PR #27

lib/Auth/Process/AttributeAddUsersGroups.php
@@ -389,8 +389,8 @@ class AttributeAddUsersGroups extends BaseFilter
             }
 
             // AD queries also seem to return the objects dn by default
-            if (isset($entry['return'])) {
-                $groups[] = $entry['return'];
+            if (isset($entry['dn'])) {
+                $groups[] = $entry['dn'];
                 continue;
             }
 