cd5bd5e74bda461f2e767734e98a67cdb3099dbd
Tim van Dijen <tvdijen@gmail.com>
2022-05-23 21:58:43 +0200

    Fix warning when an attribute is empty (#16)
    * fix warning when an attribute is empty
    
    * check if attribute value is an array
    
    Co-authored-by: brousekp <58169464+brousekp@users.noreply.github.com>

--- lib/Auth/Process/AttributeAddFromLDAP.php
+++ lib/Auth/Process/AttributeAddFromLDAP.php
@@ -95,7 +95,7 @@ class AttributeAddFromLDAP extends BaseFilter
         foreach ($attributes as $attr => $val) {
             $arrSearch[] = '%' . $attr . '%';
 
-            if (strlen($val[0]) > 0) {
+            if (is_array($val) && count($val) > 0 && strlen($val[0]) > 0) {
                 $arrReplace[] = $this->connector->escapeFilterValue($val[0], true);
             } else {
                 $arrReplace[] = '';
